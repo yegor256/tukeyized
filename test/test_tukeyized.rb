c43801a71bbcf6ec5dabee2fa2a36b0e9cbb83eb
Yegor Bugayenko <yegor256@gmail.com>
2025-10-02 08:15:15 +0300

    Refactor test names for clarity.

--- test/test_tukeyized.rb
+++ test/test_tukeyized.rb
@@ -11,13 +11,17 @@ require_relative '../lib/tukeyized'
 # Copyright:: Copyright (c) 2025 Yegor Bugayenko
 # License:: MIT
 class TestWinsorized < Minitest::Test
-  def test_simple
+  def test_filters_big_numbers
     assert_equal([1, 3, 2, 5], [1, 3, 2, 5, 800].tukeyized)
     assert_equal([1, 6, 3, 3, 2, 8], [1, 6, 3, 8888, 3, 2, 8, -19_292].tukeyized)
     assert_equal([3, 2, 1], [3, 2, 1].tukeyized)
   end
 
-  def test_empty_array
+  def test_passes_empty_array
     assert_empty([].tukeyized)
+  end
+
+  def test_passes_short_array
+    assert_equal([1, 33_333], [1, 33_333].tukeyized)
   end
 end
